aa79c3a4cc52a4c6d392ad1f89b330d12fc40f81
salah-eddine-B <b.salahbouziane@gmail.com>
2025-09-27 13:55:44 +0100

    i make the layout responsive and i have implement the minimized side bar theme to it

--- client/src/Layout/Layout.jsx
+++ client/src/Layout/Layout.jsx
@@ -8,7 +8,7 @@ export default function Layout() {
   const isMinimized = useSelector(state => state.theme.isMinimized);
 
   return (
-    <div className='main-app'>
+    <div className={`main-app ${isMinimized? 'minimized' : ''}`}>
         <div className={`sidebar-layout ${isMinimized? 'minimized' : ''}`}>i'm the sidebar</div>
         <div className='header-layout'>i'm the header</div>
         <div className='pages-layout'>i'm the pages container</div>
